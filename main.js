4ab7ebc81cf33f70938fdd6e5e22920a07ac4a59
Sikor08 <sikor08@gmail.com>
2022-12-28 11:20:32 +0300

    base content version

---
+++ main.js
@@ -0,0 +1,7 @@
+const burgerIconEl = document.querySelector('.fa-bars');
+
+const burgerListEl =document.querySelector('.burgerContent');
+
+burgerIconEl.addEventListener('click', () => {
+    burgerListEl.classList.toggle('invisible')
+})
