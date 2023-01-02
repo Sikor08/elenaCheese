ddb45ed824593f4dde02cbfb1f37226858085bd4
Sikor08 <sikor08@gmail.com>
2023-01-02 16:43:24 +0300

    catalog created

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ const burgerListEl =document.querySelector('.burgerContent');
 burgerIconEl.addEventListener('click', () => {
     burgerListEl.classList.toggle('invisible')
 })
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
