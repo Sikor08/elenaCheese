0a752bc4e1638c0f3542956db55c91f2e15cea99
Sikor08 <sikor08@gmail.com>
2023-01-21 17:40:04 +0300

    renderProduct()

--- main.js
+++ main.js
@@ -6,37 +6,67 @@ burgerIconEl.addEventListener('click', () => {
     burgerListEl.classList.toggle('invisible')
 })
 
-const btnMinusEl = document.querySelector('.btnMinus');
-btnMinusEl.setAttribute('disabled', '');
-const btnPlusEl = document.querySelector('.btnPlus');
-const stepperinputEl = document.querySelector('#stepperInput');
-
-stepperinputEl.value = '1';
-
-let count = stepperinputEl.value
-stepperinputEl.addEventListener('keyup', (e) => {
-    if(stepperinputEl.value >= 10){
-        stepperinputEl.value = 10
+const createProduct = (img, title, price, oldPrice) => {
+    return {
+        id: crypto.randomUUID(),
+        img,
+        title,
+        price,
+        oldPrice
     }
-})
+}
 
-btnPlusEl.addEventListener('click', () => {
-    count ++ 
-    stepperinputEl.value = count;
-    if(count > 1){
-        btnMinusEl.removeAttribute('disabled', '');
-    }
-    if(count >= 10){
-        btnPlusEl.setAttribute('disabled', '');
-    }
-})
-btnMinusEl.addEventListener('click', () => {
-    count -- 
-    stepperinputEl.value = count;
-    btnPlusEl.removeAttribute('disabled', '');
+const products = [
+    createProduct('img/good1.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good2.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good3.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good4.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good5.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good6.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good7.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good8.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+    createProduct('img/good9.jpg', 'Бри', `$ ${350} `, `$ ${400}`),
+]
+console.log(products)
+const renderProduct =() => {
+    for(let product of products){
+const productItemEl = document.createElement('div');
+        productItemEl.classList.add('cardItem');
+        productItemEl.setAttribute('id', product.id);
 
-    if(count <= 1){
-        btnMinusEl.setAttribute('disabled', '');
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.img);
+        productImg.classList.add('cardItem__img')
+        productItemEl.append(productImg);
 
-    }
-})
+        const productTitleEl = document.createElement('p');
+        productTitleEl.classList.add('cardItem__title');
+        productTitleEl.textContent = product.title; 
+
+        const productPriceEl = document.createElement('div');
+        productPriceEl.classList.add('cardItem__price');
+        productPriceEl.textContent = product.price;
+        const oldPriceEl = document.createElement('span');
+        oldPriceEl.classList.add('cardItem__Price');
+        oldPriceEl.classList.add('cardItem__price_old');
+        oldPriceEl.textContent = product.oldPrice;
+        productPriceEl.append(oldPriceEl)
+      
+        const productBtn = document.createElement('button');
+        productBtn.classList.add('cardItem__btn', 'buyBtn');
+        productBtn.textContent = 'Купить';
+        
+        const cardItemContent = document.createElement('div');
+        cardItemContent.classList.add('cardContent');
+        cardItemContent.append(productTitleEl, productPriceEl, productBtn);
+        productItemEl.append(cardItemContent)
+
+
+
+
+        const cardBoxEl = document.querySelector('.catalogWrap')
+
+        cardBoxEl.append(productItemEl);   
+     }
+}
+renderProduct()
