e2cd9c6cf581165bd3a790148b9ed39bb1a12351
Sikor08 <sikor08@gmail.com>
2023-02-13 20:05:09 +0300

    1302

---
+++ main.js
@@ -0,0 +1,5 @@
+const burgerIcon = document.querySelector('.burger__icon');
+const burgerContent = document.querySelector('.burgerContent')
+burgerIcon.addEventListener('click', () => {
+    burgerContent.classList.toggle('invisible')
+})
---
+++ task.md
@@ -0,0 +1,2 @@
+1 layout 
+2 search
