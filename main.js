4227b0f6846769f43c5a57e6fc43cbbb20f6b078
Sikor08 <sikor08@gmail.com>
2023-02-25 16:33:22 +0300

    2502

--- main.js
+++ main.js
@@ -4,11 +4,3 @@ burgerIcon.addEventListener('click', () => {
     burgerContent.classList.toggle('invisible')
 })
 
-const cards = document.querySelectorAll('.cardItem');
-console.log(cards);
-cards.forEach(card => {
-    const id = crypto.randomUUID();
-    card.setAttribute('id', id)  
-});
-
-const catalog = document.querySelector('.catalogWrap');
